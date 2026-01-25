104af0de607c28157af3c9858d80b039dda3af3c
shiva gupta <shivagupta1208@gmail.com>
2026-01-25 11:08:22 +0530

    Add VPC using Terraform module

---
+++ tf-module-vpc/network.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.0"
+
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = data.aws_availability_zones.available.names
+  public_subnets = ["10.0.0.0/24"]
+  private_subnets = ["10.0.1.0/24"]
+
+  tags = {
+    Name = "MyVPCModule"
+  }
+}
