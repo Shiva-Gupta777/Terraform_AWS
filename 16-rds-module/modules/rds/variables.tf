abcb9c89ecc2daaedfba49b33c3a72b95a0fab66
shiva gupta <shivagupta1208@gmail.com>
2026-02-19 09:48:12 +0530

    creating networking input such as subnets and security group ids

--- 16-rds-module/modules/rds/variables.tf
+++ 16-rds-module/modules/rds/variables.tf
@@ -1,10 +1,17 @@
+
+## General information ##
+
 variable "project_name" {
-  type = string
+  type        = string
+  description = "The project name. Used to name the RDS instance and add relevant tags."
 }
 
+## DB configuration ##
+
 variable "instance_class" {
-  type = string
-  default = "db.t3.micro"
+  type        = string
+  default     = "db.t3.micro"
+  description = "The instance class used to create the RDS instance. Requires a free-tier instance class."
 
   validation {
     condition     = contains(["db.t3.micro"], var.instance_class)
@@ -12,10 +19,10 @@ variable "instance_class" {
   }
 }
 
-
 variable "storage_size" {
-  type = number
-  default = 10
+  type        = number
+  default     = 10
+  description = "The amount of storage to allocate to the RDS instance. Should be between 5GB and 10GB."
 
   validation {
     condition     = var.storage_size >= 5 && var.storage_size <= 10
@@ -25,8 +32,10 @@ variable "storage_size" {
 
 
 variable "engine" {
-  type = string
-  default = "postgres_latest"
+  type        = string
+  default     = "postgres_latest"
+  description = "Which engine to use for the RDS instance. Currently only postgres is supported."
+
 
   validation {
     condition     = contains(["postgres_latest", "postgres14"], var.engine)
@@ -35,23 +44,26 @@ variable "engine" {
 }
 
 
+## DB credentials ##
+
 variable "credentials" {
   type = object({
     username = string
     password = string
   })
 
-  sensitive = true
+  sensitive   = true
+  description = "The root username and password for the RDS instance creation."
 
 
   validation {
-  condition = (
-    length(regexall("[A-Za-z]+", var.credentials.password)) > 0 &&
-    length(regexall("[0-9]+", var.credentials.password)) > 0 &&
-    length(regexall("[A-Za-z0-9]{6,}", var.credentials.password)) > 0
-  )
+    condition = (
+      length(regexall("[A-Za-z]+", var.credentials.password)) > 0 &&
+      length(regexall("[0-9]+", var.credentials.password)) > 0 &&
+      length(regexall("[A-Za-z0-9]{6,}", var.credentials.password)) > 0
+    )
 
-  error_message = <<EOT
+    error_message = <<EOT
 Password must:
 1. Contain at least one letter
 2. Contain at least one digit
@@ -59,4 +71,19 @@ Password must:
 EOT
   }
 
+}
+
+
+## DB network ##
+
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs to deploy the RDS instance in"
+}
+
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "Security group IDs to attach to the RDS instance"
 }
