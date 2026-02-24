51b7e593e71ea6d240c6eff70f23e39a2576b2bd
shiva gupta <shivagupta1208@gmail.com>
2026-02-25 00:34:50 +0530

    Creating the custom security Group

--- 06-resources/README.md
+++ 06-resources/README.md
@@ -3,5 +3,5 @@
 1. {DONE} Deploy a VPC and subnet
 2. {DONE} Deploy an Internet Gateway and attach it to the VPC
 3. {DONE} Create a route table and add route to the Internet Gateway
-4.	Deploy EC2 inside the subnet
+4. {Done} Deploy EC2 inside the subnet
 5.	Associate public IP and security group 
--- 06-resources/compute.tf
+++ 06-resources/compute.tf
@@ -4,6 +4,10 @@ resource "aws_instance" "ubuntu" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public.id
 
+  vpc_security_group_ids = [
+    aws_security_group.public_http_traffic.id
+  ]
+
   root_block_device {
     delete_on_termination = true
     volume_size           = 10
@@ -11,7 +15,42 @@ resource "aws_instance" "ubuntu" {
 
   }
 
-   tags = {
-      Name = "VPC-EC2-TF"
-    }
+   tags = merge(local.common_tags, {
+    Name = "VPC-EC2-Instance"
+  })
 }
+
+
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 80 and 443"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+
+   tags = merge(local.common_tags, {
+    Name = "SECURITY-GROUP-PUBLIC-HTTP-TRAFFIC",
+  })
+}
+
+// Allow inbound HTTP traffic from anywhere
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+
+// Allow inbound HTTPS traffic from anywhere
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
